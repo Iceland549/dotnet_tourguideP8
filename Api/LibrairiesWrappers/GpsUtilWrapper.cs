66ad9815b9018eef7e63ac9a8a4fdbe053287990
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-03-20 12:00:37 +0100

    update: make methods asynchronous in GpsUtilWrapper - Modified 2 methods `GetUserLocation` and `GetAttractions`

--- Api/LibrairiesWrappers/GpsUtilWrapper.cs
+++ Api/LibrairiesWrappers/GpsUtilWrapper.cs
@@ -12,13 +12,13 @@ public class GpsUtilWrapper : IGpsUtil
         _gpsUtil = new();
     }
 
-    public VisitedLocation GetUserLocation(Guid userId)
+    public async Task<VisitedLocation> GetUserLocationAsync(Guid userId)
     {
-        return _gpsUtil.GetUserLocation(userId);
+        return await _gpsUtil.GetUserLocationAsync(userId);
     }
 
-    public List<Attraction> GetAttractions()
+    public async Task<List<Attraction>> GetAttractionsAsync()
     {
-        return _gpsUtil.GetAttractions();
+        return await _gpsUtil.GetAttractionsAsync();
     }
 }
